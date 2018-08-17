7b5896a950fd89b7e178a33873765a5933cafdca
Maelson Marques <maellson@hotmail.com>
2018-08-17 11:34:38 -0300

    teste de push to origin maelson

--- diretorios/exemplo-01.php
+++ diretorios/exemplo-01.php
@@ -1,3 +1,14 @@
 <?php 
 
-echo "teste;" ?>
+$name = "images" ;
+
+if(!is_dir($name)){
+
+	mkdir($name);
+	echo "diretorio $name criado com sucesso!";
+
+} else{
+	echo "já existe o diretorio: $name";
+}
+
+?>
